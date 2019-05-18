172172fca1f9d2d845e92d389dd3fbaa02ad0971
Rishabh Agrawal <rishabha.1999@gmail.com>
2019-05-18 21:04:08 +0530

    Style fixes

--- js/sections/skills.js
+++ js/sections/skills.js
@@ -4,7 +4,7 @@ const skills = data => {
 
     for (let i = 0; i < data.length; i++) {
         output += `
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-6 col-sm-6 skill-sep">
         <span><strong>${data[i].skill}</strong></span>
       <div class="progress">
         <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="${data[i].level}"
